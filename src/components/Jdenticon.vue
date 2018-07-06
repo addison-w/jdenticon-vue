ad835adf5cfef7188517eb6845c5af076657d200
Addison <perhaps0660@gmail.com>
2018-07-06 17:41:24 +1000

    simple jdenticon app

---
+++ src/components/Jdenticon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type='text' @input='onInput' />
+    <div v-html='identicon'></div>
+  </div>
+</template>
+<script>
+import jdenticon from 'jdenticon';
+
+export default {
+    data() {
+      return {
+        textInput: ''
+      }
+    },
+    computed: {
+      identicon: function() {
+        return jdenticon.toSvg(this.textInput, 200);
+      }
+    },
+    methods: {
+      onInput: function(event) {
+        this.textInput = event.target.value;
+      }
+    }
+}
+</script>
+<style>
+
+</style>
